ff621ee860a658c6ffe2335f3a02c0e4fc92de07
wein98 <wein9298@hotmail.com>
2021-04-01 22:10:31 +0800

    ADDED: UserAPI, apachehttpclient dependency, loginform

--- src/main/java/com/matchingSystem/API/APIManager.java
+++ src/main/java/com/matchingSystem/API/APIManager.java
@@ -1,15 +1,12 @@
 package com.matchingSystem.API;
 
-
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 
 import javax.swing.*;
 import java.io.BufferedReader;
