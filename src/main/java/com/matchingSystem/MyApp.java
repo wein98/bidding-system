aadb181eb3ab476d631613dc24275b87215622a0
Wen Yuan <wenyuanchin229@gmail.com>
2021-04-01 17:45:28 +0800

    added testing call in myApp for MessageAPI getAll()

--- src/main/java/com/matchingSystem/MyApp.java
+++ src/main/java/com/matchingSystem/MyApp.java
@@ -1,7 +1,15 @@
 package com.matchingSystem;
 
+import java.util.ArrayList;
+
 public class MyApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // try out MessageAPI.getAll()
+        MessageAPI smtg = new MessageAPI();
+        ArrayList<Message> messages = smtg.getAll();
+        for (Message message:messages) {
+            System.out.println(message.toString()+"\n");
+        }
     }
 }
