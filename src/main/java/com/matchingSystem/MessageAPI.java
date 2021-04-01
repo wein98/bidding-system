326564ba98567f3c549591c35ceb09b9924db070
Wen Yuan <wenyuanchin229@gmail.com>
2021-04-01 15:42:01 +0800

    added MessageAPI class

---
+++ src/main/java/com/matchingSystem/MessageAPI.java
@@ -0,0 +1,30 @@
+package com.matchingSystem;
+
+import org.json.JSONObject;
+
+public class MessageAPI {
+    public JSONObject getAll(){
+
+        return new JSONObject();
+    }
+
+    public JSONObject create(){
+        return new JSONObject();
+    }
+
+    public JSONObject getById(String id){
+        return new JSONObject();
+    }
+
+    public JSONObject deleteById(String id){
+        return new JSONObject();
+    }
+
+    public JSONObject updatePartialById(String id){
+        return new JSONObject();
+    }
+
+    public String something() {
+        return "S";
+    }
+}
