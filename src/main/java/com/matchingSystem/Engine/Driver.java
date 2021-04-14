846a6a507553034a012fc5fef88f29891ba3a3a2
wein98 <wein9298@hotmail.com>
2021-04-14 17:08:03 +0800

    ADDED: driver class to start the program

--- src/main/java/com/matchingSystem/Engine/Driver.java
+++ src/main/java/com/matchingSystem/Engine/Driver.java
@@ -1,2 +1,13 @@
-package com.matchingSystem.Engine;public class Driver {
+package com.matchingSystem.Engine;
+
+import com.matchingSystem.Controller.LoginController;
+import com.matchingSystem.Model.LoginModel;
+import com.matchingSystem.View.LoginView;
+
+public class Driver {
+    public static void main(String[] args) {
+        LoginView loginView = new LoginView();
+        LoginModel loginModel = new LoginModel();
+        LoginController loginController = new LoginController(loginView, loginModel);
+    }
 }
