2986c79791a3e06f571b307f12238a78eaf41410
wein98 <wein9298@hotmail.com>
2021-05-20 12:29:32 +0800

    IGNORED: APIKey.java

--- src/main/java/com/matchingSystem/API/APIKey.java
+++
@@ -1,7 +0,0 @@
-package com.matchingSystem.API;
-
-public class APIKey {
-//    public static String API_KEY = "dCbTnjdmJpnRpb6LktcG88rWDzpkbN";
-    // api/v2
-    public static String API_KEY = "NtgJdgdgqnbTTPbnh8Qd9rfkRjMk8M";
-}
