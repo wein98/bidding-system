800bd3e00fcb657a20afb1bfb330ad39b9d44302
Wen Yuan <wenyuanchin229@gmail.com>
2021-05-27 12:52:12 +0800

    ADDED new view to display the upcoming expiring contract (a month)

---
+++ src/main/java/com/matchingSystem/View/ContractExpiryNotificationView.java
@@ -0,0 +1,58 @@
+package com.matchingSystem.View;
+
+import com.matchingSystem.Constant;
+import com.matchingSystem.ContractDev.Contract;
+import com.matchingSystem.ContractDev.ContractExpiryIterator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContractExpiryNotificationView extends javax.swing.JFrame{
+    private JLabel titleLabel;
+    private JPanel window1;
+    private JScrollPane scrollPanel;
+    private JPanel panel1;
+    private JLabel message;
+
+    public ContractExpiryNotificationView(ContractExpiryIterator iterator) {
+        setContractsNotifications(iterator);
+        initComponents();
+        this.setVisible(true);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+        setContentPane(window1);
+        pack();
+    }
+
+    private void setContractsNotifications(ContractExpiryIterator iterator){
+        panel1.removeAll();
+        scrollPanel.getViewport().setView(panel1);
+
+        if (iterator.isEmpty()) {
+            this.message.setText("No contract.");
+        } else {
+            int count = 0;
+            while (iterator.hasNext()){
+
+                if (iterator.isLast() && count == 0){
+                    this.message.setText("No contract is expiring in a month.");
+                }
+
+                JPanel itemPanel = (JPanel) iterator.next();
+
+                if(itemPanel!= null){
+                    GridBagConstraints gbc = new GridBagConstraints();
+                    gbc.gridwidth = GridBagConstraints.REMAINDER;
+                    gbc.fill = GridBagConstraints.HORIZONTAL;
+                    gbc.insets = new Insets(10, 0, 10, 0);
+                    panel1.add(itemPanel, gbc, 0);
+                    count ++;
+                }
+
+            }
+        }
+    }
+
+}
